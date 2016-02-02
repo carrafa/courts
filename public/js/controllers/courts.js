9d9209fe84ff999b9cb8b9893a95b3c302f70835
Joshua Carrafa <carrafa@gmail.com>
2016-02-02 00:55:56 -0500

    adds map styles, adds court list view

--- public/js/controllers/courts.js
+++ public/js/controllers/courts.js
@@ -24,7 +24,121 @@ ctrl.controller('courts', ['$scope', 'courtsApi', 'NgMap', '$rootScope',
     })
 
 
-    // $scope.getCourts();
+    $scope.getCourts();
+
+    $scope.styles = [{
+      "featureType": "landscape.man_made",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#ebeae3"
+      }]
+    }, {
+      "featureType": "landscape.natural",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#bfd74a"
+      }]
+    }, {
+      "featureType": "landscape.natural.terrain",
+      "elementType": "geometry",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "poi",
+      "elementType": "labels",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "poi.business",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "poi.government",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "poi.medical",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#fbd3da"
+      }, {
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#a3cd39"
+      }]
+    }, {
+      "featureType": "poi.school",
+      "elementType": "all",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "labels",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#fdea9b"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#ffd200"
+      }]
+    }, {
+      "featureType": "road.arterial",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#ffffff"
+      }]
+    }, {
+      "featureType": "road.local",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "visibility": "on"
+      }, {
+        "color": "black"
+      }]
+    }, {
+      "featureType": "transit.station.airport",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#fbad17"
+      }]
+    }, {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#68bad2"
+      }]
+    }]
 
   }
 ])
